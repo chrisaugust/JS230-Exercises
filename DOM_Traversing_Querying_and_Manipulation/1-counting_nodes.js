008f8ea273cc315c2df8b7ab49a04249f1810139
Chris Danielson <christopher.a.danielson@protonmail.com>
2024-11-08 13:27:07 -0500

    Sync repos

--- DOM_Traversing_Querying_and_Manipulation/1-counting_nodes.js
+++ DOM_Traversing_Querying_and_Manipulation/1-counting_nodes.js
@@ -1,38 +1,38 @@
-<div>
-  <p>Then press the <em>Draw</em>
-button</p>
-</div>
-
-Nodes:
-
-1      HTML
-       /  \
-2,3  HEAD  BODY
-            |
-4          DIV
-          / | \
-5,     TEXT |  TEXT
-            |
-7           P
-          / | \
-8,9,10 TEXT EM TEXT
-            |
-11         TEXT
-
-
-
-<div><p>Then press the <em>Draw</em
-button</p>
-</div>
-
-1         HTML
-          /  \
-2,3    HEAD  BODY
-              |
-4            DIV
-              |  
-5             P
-            / | \
-6,7,8    TEXT EM  TEXT
-              |
-9            TEXT
+// <div>
+//   <p>Then press the <em>Draw</em>
+// button</p>
+// </div>
+// 
+// Nodes:
+// 
+// 1      HTML
+//        /  \
+// 2,3  HEAD  BODY
+//             |
+// 4          DIV
+//           / | \
+// 5,     TEXT |  TEXT
+//             |
+// 7           P
+//           / | \
+// 8,9,10 TEXT EM TEXT
+//             |
+// 11         TEXT
+// 
+// 
+// 
+// <div><p>Then press the <em>Draw</em
+// button</p>
+// </div>
+// 
+// 1         HTML
+//           /  \
+// 2,3    HEAD  BODY
+//               |
+// 4            DIV
+//               |  
+// 5             P
+//             / | \
+// 6,7,8    TEXT EM  TEXT
+//               |
+// 9            TEXT
